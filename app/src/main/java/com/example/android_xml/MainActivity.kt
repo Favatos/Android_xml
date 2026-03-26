f27ea658c770c17ff86ce1f7675554007d2876aa
Favatos <werty36401@gmail.com>
2026-03-26 18:23:55 +0300

    hw_1

--- app/src/main/java/com/example/android_xml/MainActivity.kt
+++ app/src/main/java/com/example/android_xml/MainActivity.kt
@@ -1,20 +1,82 @@
 package com.example.android_xml
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.android_xml.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    var lastTime: Long = 0
+    var diff : Long = System.currentTimeMillis()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val currentTime = System.currentTimeMillis()
+        diff = currentTime - diff
+
+        Log.d("main-activity:create", "$diff")
+
+        lastTime = currentTime
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val currentTime = System.currentTimeMillis()
+        val diff = currentTime - lastTime
+
+        Log.d("main-activity:start", "$diff")
+
+        lastTime = currentTime
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        val currentTime = System.currentTimeMillis()
+        val diff = currentTime - lastTime
+
+        Log.d("main-activity:resume", "$diff")
+
+        lastTime = currentTime
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        val currentTime = System.currentTimeMillis()
+        val diff = currentTime - lastTime
+
+        Log.d("main-activity:pause", "$diff")
+
+        lastTime = currentTime
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        val currentTime = System.currentTimeMillis()
+        val diff = currentTime - lastTime
+
+        Log.d("main-activity:stop", "$diff")
+
+        lastTime = currentTime
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        val currentTime = System.currentTimeMillis()
+        val diff = currentTime - lastTime
+
+        Log.d("main-activity:destroy", "$diff")
+
+        lastTime = currentTime
     }
 }
