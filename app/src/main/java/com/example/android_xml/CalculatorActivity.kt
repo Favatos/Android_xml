9104dd7f56552666952d0e0b56f36e14918fb738
Favatos <werty36401@gmail.com>
2026-03-31 16:02:46 +0300

    hw_2

---
+++ app/src/main/java/com/example/android_xml/CalculatorActivity.kt
@@ -0,0 +1,12 @@
+package com.example.android_xml
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.android_xml.databinding.CalculatorActivityBinding
+
+class CalculatorActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/example/android_xml/WeatherActivity.kt
@@ -0,0 +1,4 @@
+package com.example.android_xml
+
+class WeatherActivity {
+}
